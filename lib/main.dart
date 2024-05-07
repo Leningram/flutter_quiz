c5a812efca9843ea41177debe756987c87d74537
unknown <yes.kanat@gmail.com>
2024-05-07 14:26:05 +0300

    start screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:quiz_game/gradient_container.dart';
+import 'package:quiz_game/start_screen.dart';
 
 void main() {
   runApp(
     const MaterialApp(
       home: Scaffold(
-        body: GradientContainer(
+        body: StartScreen(
           beginColor: Color.fromARGB(255, 69, 111, 232),
           endColor: Color.fromARGB(255, 25, 176, 236),
         ),
--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class GradientContainer extends StatelessWidget {
-  const GradientContainer(
+class StartScreen extends StatelessWidget {
+  const StartScreen(
       {required this.beginColor, required this.endColor, super.key});
 
   final Color beginColor;
@@ -20,17 +20,23 @@ class GradientContainer extends StatelessWidget {
         child: Column(
           mainAxisSize: MainAxisSize.min,
           children: [
-            Image.asset('assets/images/quiz-logo.png', width: 300),
+            Image.asset(
+              'assets/images/quiz-logo.png',
+              width: 300,
+              color: const Color.fromARGB(150, 255, 255, 255),
+            ),
             const SizedBox(height: 60),
             const Text('Learn Flutter the fun way!',
-                style: TextStyle(color: Colors.white, fontSize: 24)),
+                style: TextStyle(
+                    color: Color.fromARGB(255, 207, 230, 247), fontSize: 24)),
             const SizedBox(height: 60),
-            ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                    textStyle: const TextStyle(fontSize: 16),
-                    backgroundColor: Colors.blue[800]),
+            OutlinedButton.icon(
+                style: OutlinedButton.styleFrom(
+                    foregroundColor: Colors.white,
+                    side: const BorderSide(color: Colors.white)),
                 onPressed: () {},
-                child: const Text(
+                icon: const Icon(Icons.arrow_right),
+                label: const Text(
                   'Start Quiz',
                   style: TextStyle(color: Colors.white),
                 ))
