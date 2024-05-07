8b2e25e23e9677c34aab040736b71c5896edd01b
unknown <yes.kanat@gmail.com>
2024-05-07 13:44:04 +0300

    layout added

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -6,6 +6,7 @@ class GradientContainer extends StatelessWidget {
 
   final Color beginColor;
   final Color endColor;
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -14,6 +15,27 @@ class GradientContainer extends StatelessWidget {
             colors: [beginColor, endColor],
             begin: Alignment.topLeft,
             end: Alignment.bottomRight),
+      ),
+      child: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Image.asset('assets/images/quiz-logo.png', width: 300),
+            const SizedBox(height: 60),
+            const Text('Learn Flutter the fun way!',
+                style: TextStyle(color: Colors.white, fontSize: 24)),
+            const SizedBox(height: 60),
+            ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    textStyle: const TextStyle(fontSize: 16),
+                    backgroundColor: Colors.blue[800]),
+                onPressed: () {},
+                child: const Text(
+                  'Start Quiz',
+                  style: TextStyle(color: Colors.white),
+                ))
+          ],
+        ),
       ),
     );
   }
